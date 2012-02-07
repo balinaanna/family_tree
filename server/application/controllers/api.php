3decf1bd3991cc548813f346d78caa41639e2faa
Bogdan Derevenets <dbodyas@gmail.com>
2012-02-07 18:05:15 +0200

    Login

--- server/application/controllers/api.php
+++ server/application/controllers/api.php
@@ -7,4 +7,15 @@ class Api extends CI_Controller {
 		$this->load->view('welcome_message');
 	}
 	
+	
+	public function login() {
+		$s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
+		$user = json_decode($s, true);
+		//$user['network'] - соц. сеть, через которую авторизовался пользователь
+		//$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
+		//$user['first_name'] - имя пользователя
+		//$user['last_name'] - фамилия пользователя
+		echo "Network:".$user['network']." ".$user['identity']." ".$user['first_name']." ".$user['last_name'];
+	}
+	
 }
