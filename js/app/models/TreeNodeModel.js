8e911639629e86dd0f05f861471444051eb253f2
Sergey Trotsenko <morror.prime@gmail.com>
2012-02-18 23:42:31 +0200

    commit for pull

--- js/app/models/TreeNodeModel.js
+++ js/app/models/TreeNodeModel.js
@@ -2,17 +2,6 @@ define([],function(){
 	return Backbone.Model.extend({
 		defaults:{
 				//tree: []
-				l_name: null,
-				f_name:null,	
-				f_id:null,	
-				m_id:null,
-				ch_ids:[],
-				spouse_id:null,
-				b_date:null,
-				d_date:null,
-				sex:null,
-				photo_url:null,
-				comment:null
 		},
 				
 		initialize: function(){
